506c033a0dd72a89c01b1f4d15f4d0ce332e3d11
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-05-24 21:14:40 -0400

    initial bcrypt set up TODO: passport

--- models/user.js
+++ models/user.js
@@ -1,13 +1,19 @@
 'use strict';
 
+const bcrypt = require ('bcrypt');
+
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   userName: { type: String,  required: true,  unique: true },
   // userEmail: { type: String,  required: true,  unique: true },
-  // password: { type: String,  required: true,  unique: true }
+  password: { type: String,  required: true,  unique: true }
 }, { timestamps: true });
 
+
+userSchema.statics.hashPassword = function(password){
+  return bcrypt.hash(password,10);
+};
 
 userSchema.set('toObject', {
   transform: function (doc, ret) {
