4057e39be9e40e40c83ed9b8722821b941a990fc
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-06-03 11:28:25 -0400

    adding needed s

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 exports.PORT = process.env.PORT || 8080;
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || ' http://objective-stonebraker-c8501a.netlify.com',
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || ' https://objective-stonebraker-c8501a.netlify.com',
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/stashApp';
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/stashApp-test';
