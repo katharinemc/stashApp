3fbea8da2852782215f31dd35a9b0b0736384ae5
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-06-08 11:25:00 -0400

    config for deploy

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 exports.PORT = process.env.PORT || 8080;
 
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || ' https://objective-stonebraker-c8501a.netlify.com',
+// exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || ' https://objective-stonebraker-c8501a.netlify.com',
 
 
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/stashApp';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://KatharineMc:Th1nkfulStu@ds245250.mlab.com:45250/stashapp';
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/stashApp-test';
 
